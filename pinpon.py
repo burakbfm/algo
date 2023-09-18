5e8733508749ee6a546523be180674b6ac0bdd6a
Burak Üstün <79836033+burakbfm@users.noreply.github.com>
2023-09-18 21:32:32 +0300

    Update pinpon.py
    pinpon son versiyon

--- pinpon.py
+++ pinpon.py
@@ -1,20 +1,20 @@
+
 from pygame import *
-
-
- #Sınıf kurucusu
+'''Gerekli Sınıflar'''
+ 
+#sprite'lar için ebeveyn sınıfı
 class GameSprite(sprite.Sprite):
-   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-       sprite.Sprite.__init__(self)
-       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #Birlikte 55,55 - seçenekler
        self.speed = player_speed
        self.rect = self.image.get_rect()
        self.rect.x = player_x
        self.rect.y = player_y
-
+ 
    def reset(self):
        window.blit(self.image, (self.rect.x, self.rect.y))
-
-
+ 
 class Player(GameSprite):
    def update_r(self):
        keys = key.get_pressed()
@@ -29,16 +29,13 @@ class Player(GameSprite):
        if keys[K_s] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
  
-
-
-#Oyun Sahnesi
+#Oyun sahnesi
 back = (200, 255, 255) #arka plan rengi (background)
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
-
-
+ 
 #oyunun durumundan sorumlu bayraklar
 game = True
 finish = False
@@ -46,13 +43,18 @@ clock = time.Clock()
 FPS = 60
  
 #bir top ve raket oluşturma   
-racket1 = Player('racket.png', 30, 200, 4, 50, 10) 
-racket2 = Player('racket.png', 520, 200, 4, 50, 10)
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
-
-speed_x = 3
-speed_y = 3
-
+ 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+ 
+speed_x = 4
+speed_y = 4
+ 
 while game:
    for e in event.get():
        if e.type == QUIT:
@@ -68,7 +70,22 @@ while game:
        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
            speed_x *= -1
            speed_y *= 1
-    
+      
+       #Top ekranın sınırlarına ulaşırsa, hareket yönünü değiştiririz
+       if ball.rect.y > win_height-50 or ball.rect.y < 0:
+           speed_y *= -1
+ 
+       #eğer top raketten daha uzağa uçtuysa, ilk oyuncu için kaybetme koşulunu belirleriz
+       if ball.rect.x < 0:
+           finish = True
+           window.blit(lose1, (200, 200))
+           game_over = True
+ 
+       #eğer top raketten daha uzağa uçtuysa, ikinci oyuncu için kaybedilme koşulunu belirleriz
+       if ball.rect.x > win_width:
+           finish = True
+           window.blit(lose2, (200, 200))
+           game_over = True
  
        racket1.reset()
        racket2.reset()
@@ -76,4 +93,3 @@ while game:
  
    display.update()
    clock.tick(FPS)
-
